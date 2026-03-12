da5010bb6429a56a58dcfa1d60fb062d2a8b409f
Michael Rose <michael@rosenetwork.net>
2026-03-11 23:30:55 -0700

    fixed tailwind to unfuck transform expect trouble later

--- resources/tailwind.config.js
+++ resources/tailwind.config.js
@@ -2,15 +2,13 @@ module.exports = {
   // Add this block to stop Tailwind from "breaking" Basecoat
   corePlugins: {
     preflight: false,
+    transform: false,
   },
   content: [
     './src/**/*',
     './resources/**/*',
   ],
-  theme: {
-    extend: {},
-  },
+theme: {},
   plugins: [
-    /* require('@tailwindcss/forms'), */
   ],
 }
